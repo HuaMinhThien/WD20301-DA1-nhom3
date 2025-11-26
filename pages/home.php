8971a824c232d64e5b245efd503a738d8800b84b
tuankudo1502-commits <tuankudo1502@gmail.com>
2025-11-26 21:37:55 +0700

    hihi

--- pages/home.php
+++ pages/home.php
@@ -47,50 +47,54 @@
 
     <div class="main-container-2">
     <!-- Layout Grid 4x3 mới -->
-    <div class="main-ctn2-promo-grid-4x3">
+        <div class="main-ctn2-promo-grid-4x3">
 
-        <!-- 1. Túi xách: cột 1-2, hàng 1 -->
-        <div class="main-ctn2-grid-item main-ctn2-item-bag">
-            <img src="assets/images/img-banner/banner-con-1.png" alt="Túi xách">
-            <div class="main-ctn2-content">
-                <h3>Túi xách</h3>
-                <p>MUA 2 SẢN PHẨM GIẢM 50%</p>
-                <a href="#" class="main-ctn2-btn">Mua ngay</a>
+            <!-- 1. Túi xách: cột 1-2, hàng 1 -->
+            <div class="main-ctn2-grid-item main-ctn2-item-bag">
+                <img src="assets/images/img-banner/banner-con-1.png" alt="Túi xách">
+                <div class="main-ctn2-content">
+                    <h3>Túi xách</h3>
+                    <p>MUA 2 SẢN PHẨM GIẢM 50%</p>
+                    <a href="#" class="main-ctn2-btn">Mua ngay</a>
+                </div>
             </div>
-        </div>
 
-        <!-- 2. Thời trang nam: cột 3-4, hàng 1-2 (cao gấp đôi) -->
-        <div class="main-ctn2-grid-item main-ctn2-item-man">
-            <img src="assets/images/img-banner/banner-con-2.png" alt="Thời trang nam">
-            <div class="main-ctn2-content">
-                <h3>Thời trang nam</h3>
-                <p>XU HƯỚNG MỚI NHẤT<br>MÙA HÈ NÀY</p>
-                <a href="#" class="main-ctn2-btn">Mua ngay</a>
+            <!-- 2. Thời trang nam: cột 3-4, hàng 1-2 (cao gấp đôi) -->
+            <div class="main-ctn2-grid-item main-ctn2-item-man">
+                <img src="assets/images/img-banner/banner-con-2.png" alt="Thời trang nam">
+                <div class="main-ctn2-content">
+                    <h3>Thời trang nam</h3>
+                    <p>XU HƯỚNG MỚI NHẤT<br>MÙA HÈ NÀY</p>
+                    <a href="#" class="main-ctn2-btn">Mua ngay</a>
+                </div>
             </div>
-        </div>
 
-        <!-- 3. Thời trang nữ: cột 1-2, hàng 2-3 (cao gấp đôi) -->
-        <div class="main-ctn2-grid-item main-ctn2-item-woman">
-            <img src="assets/images/img-banner/banner-con-3.png" alt="Thời trang nữ">
-            <div class="main-ctn2-content">
-                <h3>Thời trang nữ</h3>
-                <p>BỘ SƯU TẬP HÈ<br>MỚI NHẤT</p>
-                <a href="#" class="main-ctn2-btn">Mua ngay</a>
+            <!-- 3. Thời trang nữ: cột 1-2, hàng 2-3 (cao gấp đôi) -->
+            <div class="main-ctn2-grid-item main-ctn2-item-woman">
+                <img src="assets/images/img-banner/banner-con-3.png" alt="Thời trang nữ">
+                <div class="main-ctn2-content">
+                    <h3>Thời trang nữ</h3>
+                    <p>BỘ SƯU TẬP HÈ<br>MỚI NHẤT</p>
+                    <a href="#" class="main-ctn2-btn">Mua ngay</a>
+                </div>
             </div>
-        </div>
 
-        <!-- 4. Cho bé: cột 3-4, hàng 3 -->
-        <div class="main-ctn2-grid-item main-ctn2-item-kid">
-            <img src="assets/images/img-banner/banner-con-4.png" alt="Cho bé">
-            <div class="main-ctn2-content">
-                <h3>Cho bé</h3>
-                <p>THIẾT KẾ MỚI NHẤT<br>MÙA HÈ 2025</p>
-                <a href="#" class="main-ctn2-btn">Mua ngay</a>
+            <!-- 4. Cho bé: cột 3-4, hàng 3 -->
+            <div class="main-ctn2-grid-item main-ctn2-item-kid">
+                <img src="assets/images/img-banner/banner-con-4.png" alt="Cho bé">
+                <div class="main-ctn2-content">
+                    <h3>Cho bé</h3>
+                    <p>THIẾT KẾ MỚI NHẤT<br>MÙA HÈ 2025</p>
+                    <a href="#" class="main-ctn2-btn">Mua ngay</a>
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
-</div>
+
+    <!-- top nổi bật -->
+     <h1>hihi</h1>
+    <!-- end top nổi bật -->
 </main>
 
 <script src="assets/js/banner.js"></script>
